b4269c444719d6dfbd7d84bb9f0180c8664dfac6
cursoTestes <cfc@ufrj.br>
2014-02-12 21:24:38 -0300

    Fazendo o codigo do CalculadoraComissao implementar os requisitos

--- Vendas/src/br/com/k21/vendas/controle/CalculadoraComissao.java
+++ Vendas/src/br/com/k21/vendas/controle/CalculadoraComissao.java
@@ -2,11 +2,16 @@ package br.com.k21.vendas.controle;
 
 public class CalculadoraComissao {
 
-	public double calculaComissao(double totalVendas){
-		if(totalVendas > 10000)
-			return totalVendas*0.06;
-		else
-			return totalVendas*0.05;
+	public double calculaComissao(double totalVendas) {
+		double calculado = 0.0;
+		
+		if (totalVendas <= 10000) {
+			calculado = totalVendas * 5 / 100;
+		} else {
+			calculado = totalVendas * 6 / 100;
+		}
+		
+		return Math.floor(calculado * 100) / 100;
 	}
 	
 }
