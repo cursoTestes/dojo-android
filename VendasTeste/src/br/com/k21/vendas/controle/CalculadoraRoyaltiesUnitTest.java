9398167bdedee631a136369f19aa4031794147f1
cursoTestes <cfc@ufrj.br>
2014-02-12 21:36:37 -0300

    Acertando testes de CalculadoraRoyaltiesUnitTest

--- VendasTeste/src/br/com/k21/vendas/controle/CalculadoraRoyaltiesUnitTest.java
+++ VendasTeste/src/br/com/k21/vendas/controle/CalculadoraRoyaltiesUnitTest.java
@@ -1,48 +1,47 @@
 package br.com.k21.vendas.controle;
 
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.List;
 
 import junit.framework.TestCase;
+import br.com.k21.vendas.ws.VendasWebService;
 
+// TODO nao está correto, somente exemplo
 public class CalculadoraRoyaltiesUnitTest extends TestCase {
 
+	private VendasWebService vendasWS;
+	private CalculadoraComissao calculadoraComissao;
+
 	@Override
-	public void setUp() throws Exception {
+	protected void setUp() throws Exception {
+		vendasWS = mock(VendasWebService.class);
+		calculadoraComissao = mock(CalculadoraComissao.class);
 	}
-
-	public void testCalculaRoyaltiesVendas100Retorna20() {
-		assertTrue(true);
-		//fail();
+	
+	public void testParaUmMesSemVendas() {
+		int mes = 1;
+		int ano = 2014;
+		double esperado = 0;
+		
+		double resultado = new CalculadoraRoyalties(vendasWS, calculadoraComissao).calcularRoyalties(mes,ano);
+		
+		assertEquals(esperado, resultado);
 	}
+	
+	public void testParaUmMesComUmaVendaDe100() {
+		int mes = 1;
+		int ano = 2014;
+		double esperado = 19;
 
-	public void testComissaoVendasDe100ComissaoEsperadaDe5() {
-
-		//You can mock concrete classes, not only interfaces
-		 LinkedList mockedList = mock(LinkedList.class);
-
-		 //stubbing
-		 when(mockedList.get(0)).thenReturn("first");
-
-		 when(mockedList.get(1)).thenThrow(new RuntimeException());
-
-		 //following prints "first"
-		 System.out.println(mockedList.get(0));
-
-		 //following throws runtime exception
-//		 System.out.println(mockedList.get(1));
-
-		 //following prints "null" because get(999) was not stubbed
-		 System.out.println(mockedList.get(999));
-
-		 //Although it is possible to verify a stubbed invocation, usually it's just redundant
-		 //If your code cares what get(0) returns then something else breaks (often before even verify() gets executed).
-		 //If your code doesn't care what get(0) returns then it should not be stubbed. Not convinced? See here.
-		 verify(mockedList).get(0);
+		List<Double> vendas = new ArrayList<Double>();
+		vendas.add(100d);
+		when(vendasWS.obterVendasPorMesEAno(mes, ano)).thenReturn(vendas);
+		
+		double resultado = new CalculadoraRoyalties(vendasWS, calculadoraComissao).calcularRoyalties(mes,ano);
+		
+		assertEquals(esperado, resultado);
 	}
-
-
 }
