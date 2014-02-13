5879507b253c76dc75f25749b64f08ba7e06435c
cursoTestes <cfc@ufrj.br>
2014-02-12 21:25:08 -0300

    Parte do calculo dos Royalties pronta

--- Vendas/src/br/com/k21/vendas/controle/CalculadoraRoyalties.java
+++ Vendas/src/br/com/k21/vendas/controle/CalculadoraRoyalties.java
@@ -1,18 +1,27 @@
 package br.com.k21.vendas.controle;
 
-import br.com.k21.vendas.dao.VendaRepository;
+import java.util.List;
+
+import br.com.k21.vendas.ws.VendasWebService;
 
 public class CalculadoraRoyalties {
 
 	private CalculadoraComissao calculadoraComissao;
-    private VendaRepository vendasDAO;
+    private VendasWebService vendasWS;
 
-    public CalculadoraRoyalties(CalculadoraComissao comissao, VendaRepository dao){
-    	calculadoraComissao = comissao;
-    	vendasDAO = dao;
-    }
-	
-	public double calcularRoyalties(double totalFaturamento){
-		return 0;
+	public CalculadoraRoyalties(VendasWebService vendasWS, CalculadoraComissao calculadoraComissao) {
+		this.vendasWS = vendasWS;
+		this.calculadoraComissao = calculadoraComissao;
+	}
+
+	public double calcularRoyalties(int mes, int ano) {
+		List<Double> vendas = vendasWS.obterVendasPorMesEAno(ano, mes);
+		double faturamento = 0d;
+		
+		for(Double venda : vendas) {
+			faturamento += (venda - calculadoraComissao.calculaComissao(venda));			
+		}
+		
+		return faturamento * 0.2;
 	}
 }
